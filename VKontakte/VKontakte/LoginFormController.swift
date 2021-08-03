f8b2e0b67c55380805fc31e30da61c4465d99b40
Swordinmouth <valeriy.kantemir@gmail.com>
2021-08-03 10:12:39 +0300

    init

---
+++ VKontakte/VKontakte/LoginFormController.swift
@@ -0,0 +1,10 @@
+// LoginFormController.swift
+// Copyright © Polina. All rights reserved.
+
+import UIKit
+
+final class LoginFormController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
